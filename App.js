8901593c423b3a3db8292ac6f786ef33b6763f50
Marcos Affonso <43121252+ProgMarcos@users.noreply.github.com>
2020-11-02 03:20:16 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -9,22 +9,18 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-import Index from './src/nav/index'
 import Login from './src/Telas/Login'
 import Quest from './src/Telas/quest'
-import ListQuest from './src/Telas/ListQuest'
 import Resultado from './src/Telas/resultado'
-import quest from './src/Telas/quest';
-
-
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/Telas/tabs'
 
 export default class App extends Component{
-  
   render(){
     return(
-      <SafeAreaView style={styles.container}>
-          <Resultado/>
-      </SafeAreaView>
+      <NavigationContainer>
+          <Tabs/>
+      </NavigationContainer>
     )
   }
 }
